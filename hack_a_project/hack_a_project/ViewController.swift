b0531183f531e6551b336cfa7e0460444f33bc1a
Wei Loon <47574881+cwlroda@users.noreply.github.com>
2020-02-29 14:43:26 +0000

    linked database

--- hack_a_project/hack_a_project/ViewController.swift
+++ hack_a_project/hack_a_project/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 class ViewController: UIViewController {
-
+    var db: Firestore!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // [START setup]
+        let settings = FirestoreSettings()
+
+        Firestore.firestore().settings = settings
+        // [END setup]
+        db = Firestore.firestore()
     }
+    
+    private func getDocument() {
+        // [START get_document]
+        let docRef = db.collection("giving").document("cwlroda")
 
-
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document does not exist")
+            }
+        }
+        // [END get_document]
+    }
 }
 
