32850a76aa6ba0af3c117c50f0bc173e0700a3be
claralebbos <claralebbos@hotmail.com>
2020-02-29 14:42:20 +0000

    Added files to my branch

--- hack_a_project/hack_a_project/RadiusController.swift
+++ hack_a_project/hack_a_project/RadiusController.swift
@@ -1,44 +1,33 @@
 //
-//  ViewController.swift
+//  RadiusController.swift
 //  hack_a_project
 //
 //  Created by Clara Lebbos on 2/29/20.
 //  Copyright © 2020 Clara Lebbos. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    @IBOutlet weak var field: UITextField!
+class RadiusController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var pickerData: UIPickerView!
-    
-    
     var data : [String] = [String]()
-    var iconClick = true
 
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerData?.delegate = self
         pickerData?.dataSource = self
-        data = ["Give", "Request", "Too Good To Go"]
+        data = ["1km", "2km", "5km", "10km"]
     }
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return data.count
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return data[row]
     }
-    
-    @IBAction func login(_ sender: Any) {
-        let text: String = field.text ?? ""
-        Singleton.getSharedState().name = text
-    }
-
-
 }
-
