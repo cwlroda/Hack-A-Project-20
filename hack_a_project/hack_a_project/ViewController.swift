8d470d075046cf4c54b61cd2bfc5e12946556242
claralebbos <claralebbos@hotmail.com>
2020-02-29 11:40:51 +0000

    Adding correct xcode project

---
+++ hack_a_project/hack_a_project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hack_a_project
+//
+//  Created by Clara Lebbos on 2/29/20.
+//  Copyright © 2020 Clara Lebbos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
